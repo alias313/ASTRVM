9a6f45500317dad088de50fc87dd83da76125d41
Elies Garcia Alvira <68759092+ele-is@users.noreply.github.com>
2022-01-27 00:10:16 +0100

    Create TRANS_TELECOS.ino

---
+++ TRANS_TELECOS.ino
@@ -0,0 +1,57 @@
+/*********
+  Modified from the examples of the Arduino LoRa library
+  More resources: https://randomnerdtutorials.com
+*********/
+
+#include <SPI.h>
+#include <LoRa.h>
+
+//define the pins used by the transceiver module
+#define ss 5
+#define rst 14
+#define dio0 4
+
+int counter = 0;
+
+const int LED_BUILTIN = 2;
+
+void setup() {
+  //setup led
+  pinMode(LED_BUILTIN, OUTPUT);
+  //initialize Serial Monitor
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("Transmisor LoRa");
+
+  //setup LoRa transceiver module
+  LoRa.setPins(ss, rst, dio0);
+  
+  //replace the LoRa.begin(---E-) argument with your location's frequency 
+  //433E6 for Asia
+  while (!LoRa.begin(433E6)) {
+    Serial.println(".");
+    delay(500);
+  }
+   // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(0xF3);
+  Serial.println("Inicializacion LoRa OK!");
+}
+
+void loop() {
+  Serial.print("Enviando paquete: ");
+  Serial.println(counter);
+
+  //Send LoRa packet to receiver
+  LoRa.beginPacket();
+  LoRa.print("hola ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(9000);
+}
