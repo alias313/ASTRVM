c62a267c3ec7446ec7b792abf051c2c64e3bd134
Elies Garcia Alvira <68759092+ele-is@users.noreply.github.com>
2022-02-23 23:35:04 +0100

    Update BME280_test.ino

--- BME280_test.ino
+++ BME280_test.ino
@@ -17,52 +17,46 @@ Adafruit_BME280 bme; // I2C
 unsigned long delayTime;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println(F("BME280 test"));
-
+  
   bool status;
 
   // default settings
   // (you can also pass in a Wire library object like &Wire2)
   status = bme.begin(0x76);  
   if (!status) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    Serial.println("No se pudo encontrar un sensor BME280 valido, comprueba el cableado!");
     while (1);
   }
 
-  Serial.println("-- Default Test --");
+  Serial.println("-- Test --");
   delayTime = 1000;
 
   Serial.println();
 }
 
-
-void loop() { 
-  printValues();
-  delay(delayTime);
-}
-
-void printValues() {
-  Serial.print("Temperature = ");
+void printBME280() {
+  Serial.print("Temperatura = ");
   Serial.print(bme.readTemperature());
   Serial.println(" *C");
   
-  // Convert temperature to Fahrenheit
-  /*Serial.print("Temperature = ");
-  Serial.print(1.8 * bme.readTemperature() + 32);
-  Serial.println(" *F");*/
-  
-  Serial.print("Pressure = ");
+  Serial.print("Presion = ");
   Serial.print(bme.readPressure() / 100.0F);
   Serial.println(" hPa");
 
-  Serial.print("Approx. Altitude = ");
+  Serial.print("Altitud = ");
   Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
   Serial.println(" m");
 
-  Serial.print("Humidity = ");
+  Serial.print("Humedad = ");
   Serial.print(bme.readHumidity());
   Serial.println(" %");
 
   Serial.println();
+  }
+
+void loop() { 
+  printBME280();
+  delay(delayTime);
 }
